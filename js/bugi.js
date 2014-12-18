dea694cd44a0e2b11b34f54986e3b00af48704c1
Morgan McCollum <morgan.mccol@gmail.com>
2014-12-18 09:44:29 -0600

    Added payment choice options and background css

--- js/bugi.js
+++ js/bugi.js
@@ -1,6 +1,30 @@
 ﻿$(document).ready(function () {
+    
+
+    $('#payment-type .payment-event').each(function() {
+        $(this).data('event', {
+            title: $.trim($(this).text()),
+            stick: true,
+            editable: true,
+            color: $(this).css("background-color")
+    });
+
+        $(this).draggable({
+            zIndex: 999,
+            revert: true,
+            revertDuration: 0,
+    });
+
+    });
 
     $('#calendar').fullCalendar({
-    })
-
+        header: {
+            left: 'prev, next, title',
+            right: 'today, month, agendaWeek, agendaDay'
+        },
+        selectable: true,
+        selectHelper: true,
+        droppable: true,
+        eventLimit: true
+    });
 });
