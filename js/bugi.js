36aaa8de23de3db614f52df628089f6b0132d96a
Morgan McCollum <morgan.mccol@gmail.com>
2015-01-05 04:28:23 -0600

    Working on menu problem, not solved

--- js/bugi.js
+++ js/bugi.js
@@ -1,4 +1,7 @@
 ﻿$(document).ready(function () {
+    var lossArray = [0];
+    var gainArray = [0];
+
     $("#payment-type .payment-event").each(function () {
         $(this).data("event", {
             title: $.trim($(this).text()),
@@ -25,17 +28,33 @@
         droppable: true,
         eventLimit: true,
         eventClick: function (event, element) {
-            var paymentForm = '<div class="field"><label for="paymentName">Name: </label><input type="text" name="paymentName" id="paymentName" value="" maxlength="14"/></div>' +
-					'<div class="field"><label for="paymentAmount">Amount: </label><input type="number" name="paymentAmount" id="paymentAmount" value="0" /></div>';
+            if  (!$(event)){
+                var paymentForm = '<div class="field"><label for="paymentName">Name: </label><input type="text" name="paymentName" id="paymentName" value="" maxlength="14"/></div>' +
+					'<div class="field"><label for="gainAmount">Amount: </label><input type="number" name="gainAmount" id="paymentAmount" value="0" min="0" /></div>';
+            }
 
+            else {
+                var paymentForm = '<div class="field"><label for="paymentName">Name: </label><input type="text" name="paymentName" id="paymentName" value="" maxlength="14"/></div>' +
+					'<div class="field"><label for="lossAmount">Amount: </label><input type="number" name="lossAmount" id="paymentAmount" value="0" min="0" /></div>';
+            }
+           
             paymentState = {
                 state0: {
                     title: 'Enter payment information:',
                     html: paymentForm,
-                    buttons: { Cancel: false, Submit: true },
-                    focus: 1,
+                    buttons: { Submit: 1 },
                     submit: function (e, v, m, f) {
-                        event.title = f.paymentName + " " + "$" + f.paymentAmount + ".00";
+                        if ($(event)) {
+                            event.title = f.paymentName + " " + "$" + f.gainAmount + ".00";
+                            $("#expense").html("$" + f.gainAmount + ".00");
+                            
+                        }
+
+                        else {
+                           event.title = f.paymentName + " " + "$" + f.lossAmount + ".00";
+                            $("#income").html("$" + f.lossAmount + ".00");
+                        }
+                        
                         $("#calendar").fullCalendar("updateEvent", event);
                         $.prompt.close();
                     }
