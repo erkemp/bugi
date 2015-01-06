caca9329080a094c522986fa92aaaf6920cb0bc4
Morgan McCollum <morgan.mccol@gmail.com>
2015-01-06 04:17:25 -0600

    Gain/loss panel complete

--- js/bugi.js
+++ js/bugi.js
@@ -1,4 +1,15 @@
-﻿$(document).ready(function () {
+﻿Number.prototype.formatMoney = function (c, d, t) {
+    var n = this,
+        c = isNaN(c = Math.abs(c)) ? 2 : c,
+        d = d == undefined ? "." : d,
+        t = t == undefined ? "," : t,
+        s = n < 0 ? "-" : "",
+        i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+
+$(document).ready(function () {
     var lossArray = [0];
     var gainArray = [0];
 
@@ -26,16 +37,16 @@
         selectHelper: true,
         droppable: true,
         eventLimit: true,
-        eventClick: function (event, element) {
+        eventClick: function(event, element) {
             if ($(this).css("background-color") == "rgb(60, 179, 113)")
                 $(this).addClass("income");
 
             if ($(this).is(".income")) {
                 var paymentForm = '<div class="field"><label for="paymentName">Name: </label><input type="text" name="paymentName" id="paymentName" value="" maxlength="14"/></div>' +
-                                  '<div class="field"><label for="gainAmount">Amount: </label><input type="number" name="gainAmount" id="paymentAmount" value="0" min="0" /></div>';
+                    '<div class="field"><label for="gainAmount">Amount: </label><input type="number" name="gainAmount" id="paymentAmount" value="0" min="0" /></div>';
             } else {
                 var paymentForm = '<div class="field"><label for="paymentName">Name: </label><input type="text" name="paymentName" id="paymentName" value="" maxlength="14"/></div>' +
-                                  '<div class="field"><label for="lossAmount">Amount: </label><input type="number" name="lossAmount" id="paymentAmount" value="0" min="0" /></div>';
+                    '<div class="field"><label for="lossAmount">Amount: </label><input type="number" name="lossAmount" id="paymentAmount" value="0" min="0" /></div>';
             }
 
             paymentState = {
@@ -43,20 +54,36 @@
                     title: 'Enter payment information:',
                     html: paymentForm,
                     buttons: { Submit: 1 },
-                    submit: function (e, v, m, f) {
+                    submit: function(e, v, m, f) {
 
                         if (f.lossAmount) {
                             var lossTotal = 0;
-                            event.title = f.paymentName + " " + "$" + f.lossAmount + ".00";
+                            event.title = f.paymentName + " " + "$" + f.lossAmount;
+                            f.lossAmount *= -1;
                             lossArray.push(f.lossAmount);
-                            $.each(lossArray, function () { lossTotal += parseFloat(this) || 0; });
-                            $("#expensePanel").html("$" + lossTotal + ".00");
+                            $.each(lossArray, function() { lossTotal -= parseFloat(this) || 0; });
+                            $("#expensePanel").html("$" + (lossTotal.formatMoney(2)));
                         } else {
                             var gainTotal = 0;
-                            event.title = f.paymentName + " " + "$" + f.gainAmount + ".00";
+                            event.title = f.paymentName + " " + "$" + f.gainAmount;
                             gainArray.push(f.gainAmount);
-                            $.each(gainArray, function () { gainTotal += parseFloat(this) || 0; });
-                            $("#incomePanel").html("$" + gainTotal + ".00");
+                            $.each(gainArray, function() { gainTotal += parseFloat(this) || 0; });
+                            $("#incomePanel").html("$" + (gainTotal.formatMoney(2)));
+                        }
+
+                        var gainlossTotal = 0;
+                        var gainlossArray = gainArray.concat(lossArray);
+                        $.each(gainlossArray, function () { gainlossTotal += parseFloat(this) || 0; });
+
+                        if (gainlossTotal > 0) {
+                            $(".lossHide").hide();
+                            $(".gainHide").show();
+                            $("#gainPanel").html("$" + (gainlossTotal.formatMoney(2)));
+                        }
+                        else {
+                            $(".gainHide").hide();
+                            $(".lossHide").show();
+                            $("#lossPanel").html("$" + (gainlossTotal.formatMoney(2)));
                         }
 
                         $("#calendar").fullCalendar("updateEvent", event);
