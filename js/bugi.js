0a9d44f185c3cab6b32d6ce486925072be18f593
Morgan McCollum <morgan.mccol@gmail.com>
2015-01-06 03:14:36 -0600

    Menu problem solved w/ income and expense panels

--- js/bugi.js
+++ js/bugi.js
@@ -15,7 +15,6 @@
             revert: true,
             revertDuration: 0,
         });
-
     });
 
     $("#calendar").fullCalendar({
@@ -28,39 +27,43 @@
         droppable: true,
         eventLimit: true,
         eventClick: function (event, element) {
-            if  (!$(event)){
+            if ($(this).css("background-color") == "rgb(60, 179, 113)")
+                $(this).addClass("income");
+
+            if ($(this).is(".income")) {
                 var paymentForm = '<div class="field"><label for="paymentName">Name: </label><input type="text" name="paymentName" id="paymentName" value="" maxlength="14"/></div>' +
-					'<div class="field"><label for="gainAmount">Amount: </label><input type="number" name="gainAmount" id="paymentAmount" value="0" min="0" /></div>';
+                                  '<div class="field"><label for="gainAmount">Amount: </label><input type="number" name="gainAmount" id="paymentAmount" value="0" min="0" /></div>';
+            } else {
+                var paymentForm = '<div class="field"><label for="paymentName">Name: </label><input type="text" name="paymentName" id="paymentName" value="" maxlength="14"/></div>' +
+                                  '<div class="field"><label for="lossAmount">Amount: </label><input type="number" name="lossAmount" id="paymentAmount" value="0" min="0" /></div>';
             }
 
-            else {
-                var paymentForm = '<div class="field"><label for="paymentName">Name: </label><input type="text" name="paymentName" id="paymentName" value="" maxlength="14"/></div>' +
-					'<div class="field"><label for="lossAmount">Amount: </label><input type="number" name="lossAmount" id="paymentAmount" value="0" min="0" /></div>';
-            }
-           
             paymentState = {
                 state0: {
                     title: 'Enter payment information:',
                     html: paymentForm,
                     buttons: { Submit: 1 },
                     submit: function (e, v, m, f) {
-                        if ($(event)) {
+
+                        if (f.lossAmount) {
+                            var lossTotal = 0;
+                            event.title = f.paymentName + " " + "$" + f.lossAmount + ".00";
+                            lossArray.push(f.lossAmount);
+                            $.each(lossArray, function () { lossTotal += parseFloat(this) || 0; });
+                            $("#expensePanel").html("$" + lossTotal + ".00");
+                        } else {
+                            var gainTotal = 0;
                             event.title = f.paymentName + " " + "$" + f.gainAmount + ".00";
-                            $("#expense").html("$" + f.gainAmount + ".00");
-                            
+                            gainArray.push(f.gainAmount);
+                            $.each(gainArray, function () { gainTotal += parseFloat(this) || 0; });
+                            $("#incomePanel").html("$" + gainTotal + ".00");
                         }
 
-                        else {
-                           event.title = f.paymentName + " " + "$" + f.lossAmount + ".00";
-                            $("#income").html("$" + f.lossAmount + ".00");
-                        }
-                        
                         $("#calendar").fullCalendar("updateEvent", event);
                         $.prompt.close();
                     }
                 }
             };
-
             $.prompt(paymentState);
         }
     });
@@ -79,24 +82,3 @@ $(document).ready(function () {
         $("#loginPanel").slideToggle("slow");
     });
 });
-
-function paymentEventUpdate() {
-    var paymentForm = '<div class="field"><label for="paymentName">Name</label><input type="text" name="paymentName" id="paymentName" value="" /></div>' +
-					'<div class="field"><label for="paymentAmount">amount</label><input type="number" name="paymentAmount" id="paymentAmount" value="0" /></div>';
-
-    paymentState = {
-        state0: {
-            title: 'Enter payment information:',
-            html: paymentForm,
-            buttons: { Cancel: false, Submit: true },
-            focus: 1,
-            submit: function (e, v, m, f) {
-                event.title = f.paymentName;
-                $("#calendar").fullCalendar("updateEvent", event);
-                $.prompt.close();
-            }
-        }
-    };
-
-    $.prompt(paymentState);
-}
